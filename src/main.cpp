599ab6896cf877879b63402a2b929fbe5b484e20
Владислав Дума <vtb4066841@vtb.ru>
2023-06-19 14:48:45 +0300

    Keyboard added + mouse clicks

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,25 @@
 #include <Arduino.h>
 #include <Mouse.h>
+#include <Keyboard.h>
 
 void setup() {
   // put your setup code here, to run once:
   Mouse.begin();
+  Keyboard.begin();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   Mouse.move(10,10,0);
-  delay(1000);
+  Mouse.click();
+  long charAscii = random(32, 127);
+  Keyboard.press(charAscii);
+  Keyboard.releaseAll();
+  delay(random(200, 1000));
   Mouse.move(-10,-10,0);
-  delay(1000);
+  Mouse.click();
+  charAscii = random(32, 127);
+  Keyboard.press(charAscii);
+  Keyboard.releaseAll();
+  delay(random(200, 1000));
 }
